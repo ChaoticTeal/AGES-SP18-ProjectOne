647460e8016322af1bcac892e7d969e2d633c3e7
Joshua Whittom <Joshua.Whittom@iam.local>
2018-02-12 15:21:00 -0600

    Disco Mode Test
    Tests have been run on the disco mode setting. Results are fantastic.

---
+++ Assets/Scripts/CameraDiscoTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDiscoTest : MonoBehaviour 
+{
+    Camera camera;
+	// Use this for initialization
+	void Start () 
+	{
+        camera = GetComponent<Camera>();
+        StartCoroutine(DiscoModeCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private IEnumerator DiscoModeCoroutine()
+    {
+        float h, s, v;
+        while (true)
+        {
+            Color.RGBToHSV(camera.backgroundColor, out h, out s, out v);
+            h += .005f;
+            if (h == 1f)
+                h = 0f;
+            camera.backgroundColor = Color.HSVToRGB(h, s, v);
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LightDiscoTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDiscoTest : MonoBehaviour 
+{
+    Light light;
+	// Use this for initialization
+	void Start () 
+	{
+        light = GetComponent<Light>();
+        StartCoroutine(DiscoModeCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private IEnumerator DiscoModeCoroutine()
+    {
+        float h, s, v;
+        while (true)
+        {
+            Color.RGBToHSV(light.color, out h, out s, out v);
+            h += .01f;
+            if (h == 1f)
+                h = 0f;
+            light.color = Color.HSVToRGB(h, s, v);
+            yield return null;
+        }
+    }
+
+}
