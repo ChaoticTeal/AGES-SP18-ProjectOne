b1ccdbaaf64867b8df7948a0e48a03f79519d153
Joshua Whittom <Joshua.Whittom@iam.local>
2018-02-26 15:31:32 -0600

    In-class Update 2/26
    Control and movement updated.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
+    // Brake multiplier
+    [SerializeField]
+    float brakeMultiplier = .99f;
+    // Fire cooldown
+    [SerializeField]
+    float fireCooldown = 2f;    
     // Projectile speed
     [SerializeField]
     float fireSpeed = 5f;
+    // Maximum velocity
+    [SerializeField]
+    float maxVelocity = 50f;
     // Movement speed
     [SerializeField]
     float moveSpeed = 5f;
@@ -23,6 +32,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Transform shootPoint;
 
+    // Can the player shoot?
+    bool canShoot = true;
+    // Is the player dancing?
+    bool isDancing;
     // Should the player shoot?
     bool shouldShoot;
     // Horizontal input
@@ -57,9 +70,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Rotate();
-        Move();
-        if (shouldShoot)
+        if (!isDancing)
+        {
+            Rotate();
+            Move();
+            AdjustVelocity();
+        }
+        if (shouldShoot && canShoot)
             Fire();
     }
 
@@ -74,24 +91,40 @@ public class PlayerMovement : MonoBehaviour
     // Turn the player - from the Unity TANKS tutorial
     void Rotate()
     {
-        float turn = horizontalInput * rotateSpeed * Time.deltaTime;
+        float turn = horizontalInput * rotateSpeed * Time.fixedDeltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rigidbody.angularVelocity = Vector3.zero;
         rigidbody.MoveRotation(rigidbody.rotation * turnRotation);
     }
 
-    // Move the player forward - from the Unity TANKS tutorial
+    // Move the player forward - based on the Unity TANKS tutorial
     void Move()
     {
-        Vector3 movement = transform.forward * verticalInput * moveSpeed * Time.deltaTime;
-        rigidbody.MovePosition(rigidbody.position + movement);
+        Vector3 movement = transform.forward * verticalInput * moveSpeed * Time.fixedDeltaTime;
+        rigidbody.AddForce(movement);
     }
 
+    // Create and launch a projectile
     void Fire()
     {
-        if (activeProjectile != null)
-            Destroy(activeProjectile);
         activeProjectile = Instantiate(projectile, shootPoint.position, shootPoint.transform.rotation);
         activeProjectile.transform.Rotate(Vector3.right, -90f);
+        Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), activeProjectile.GetComponent<Collider>());
         activeProjectile.GetComponent<Rigidbody>().velocity = shootPoint.transform.forward * fireSpeed;
+        StartCoroutine(ShootCooldown());
+    }
+
+    void AdjustVelocity()
+    {
+        if (Mathf.Sqrt(rigidbody.velocity.sqrMagnitude) >= maxVelocity)
+            rigidbody.velocity *= brakeMultiplier;
+    }
+
+    // Wait for cooldown
+    IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(fireCooldown);
+        canShoot = true;
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour 
+{
+    // SerializeFields for assignment in-editor
+    // Time a projectile should exist before despawning
+    [SerializeField]
+    float lifetime = 3f;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject != null)
+            Destroy(gameObject);
+    }
+}
